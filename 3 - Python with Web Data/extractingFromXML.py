831b74da369d8932e8023950e6f91b62ac5a0bb6
dat <dat52693@gmail.com>
2018-06-12 17:36:40 -0400

    Completed parsing XML

--- 3 - Python with Web Data/extractingFromXML.py
+++ 3 - Python with Web Data/extractingFromXML.py
@@ -11,15 +11,22 @@ ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter - ')
 if len(url) < 1:
-    url = 'http://py4e-data.dr-chuck.net/comments_42.xml'
+    url = 'http://py4e-data.dr-chuck.net/comments_90959.xml'
 
 xml = urllib.request.urlopen(url, context=ctx).read()
-print(xml)
 
 data = ET.fromstring(xml)
 print(data)
 
-counts = data.findall('comments/comment/count')
-print(counts)
+counts = data.findall('comments/comment')
+
+count = 0
+total = 0
+
 for item in counts:
-    print("Count:", item.find('count').text)
+    count = count + 1
+    total = total + int(item.find('count').text)
+    # print("Running total:", total)
+
+print("Count:", count)
+print("Sum:", total)

6d42a85408a3f6e1324be27c71f5dd14c7ae4094
dat <dat52693@gmail.com>
2018-08-01 13:13:39 -0400

    Course 4 - Week 6

--- ScratchPad.py
+++ ScratchPad.py
@@ -1,1 +1,11 @@
-print("This is the third time now. :)")
+def almostIncreasingSequence(sequence):
+    non_dupes = list()
+    for n in range(len(sequence)):
+        print(sequence[n])
+        if sequence[n] not in non_dupes:
+            non_dupes.append(sequence[n])
+    print("Original sequence:", sequence)
+    print("Non dupes:", non_dupes)
+    return (len(sequence) <= len(non_dupes) + 1 and non_dupes.sort() == non_dupes) or len(sequence) <= 3
+
+print(almostIncreasingSequence([1, 2, 5, 3, 5]))
